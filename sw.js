c0f9d49d5060e7b321c8e35504d7f7a3da553cb2
小金 <xiaojin@xiaojindeMacBook-Pro.local>
2026-07-21 19:19:52 +0800

    feat: add daily guided study lessons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mingli-home-v4';
+const CACHE = 'mingli-home-v8';
 const ASSETS = ['./', './index.html', './view.html', './manifest.json',
                 './icon180.png', './icon192.png', './icon512.png'];
 
